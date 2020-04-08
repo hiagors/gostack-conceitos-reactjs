a25772be201dc6eedc13b3604b2a8a5fda33e5f7
Hiago Reis Silva <hiago_reis@hotmail.com>
2020-04-08 13:40:06 -0300

    Implementação do desafio 02 GOSTACK

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = await api.post('repositories', {
+      "title" : `New repository ${Date.now()}`,
+      "url" : "www.hiago.com",
+      "techs" : ["Nodejs", "React"]
+    });
+    
+    setRepositories([...repositories, repository.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try{
+      const response = await api.delete(`repositories/${id}`);
+
+      setRepositories(repositories.filter(repository => repository.id !== id));
+    } catch(error){
+        alert('Erro ao deletar repositorio!');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <ul>
+              <li>{repository.title}</li>
+              <li>{repository.url}</li>
+              <li>{repository.techs}</li>
+            </ul>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
